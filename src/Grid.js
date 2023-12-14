3a7bbbce388575d2c89fbf484873adbcc51ccec4
Humberto <HumbertoL@users.noreply.github.com>
2023-12-14 17:38:50 -0600

    feat: attempt to rotate array

--- src/Grid.js
+++ src/Grid.js
@@ -8,47 +8,82 @@ const GridSquare = styled.div`
   border: 1px solid #ccc;
 `;
 
-// Function to split the binary string into 3-character chunks
-function chunkBinaryString(binaryString) {
-  const chunks = [];
-  for (let i = 0; i < binaryString.length; i += 3) {
-    chunks.push(binaryString.slice(i, i + 3));
-  }
-  return chunks;
+const chunkLength = 3
+
+const LED_WIDTH = 16
+// const getChunkByIndex = (index, binaryString) => {
+
+//     const startIndex = index * 3;
+//     return binaryString.substring(startIndex, startIndex + 3);
+// }
+
+const getPixelByIndex = (index, chunkArray) => {
+    const x = index % LED_WIDTH;
+    const y = Math.floor(index / LED_WIDTH);
+
+    // todo: or flip this?
+    const pixelIndex = x + (y * LED_WIDTH);
+
+    return chunkArray[pixelIndex]; 
+
 }
+
+// Function to split the binary string into 3-character chunks
+
+function chunkBinaryString(binaryString) {
+    const chunks = [];
+    for (let i = 0; i < binaryString.length; i += chunkLength) {
+        chunks.push(binaryString.slice(i, i + chunkLength));
+    }
+    return chunks;
+}
+
+function getColorFromChunk(chunk) {
+    const colors = [
+        '#000000', // Black
+        '#00FF00', // Green
+        '#FF0000', // Red   
+        '#0000FF', // Color 4
+        '#FFFF00', // Color 5
+        '#FF00FF', // Color 6
+        '#00FFFF', // Color 7
+        '#FFFFFF', // Color 8
+    ];
+
+    // Convert the chunk to a decimal number (base 2) to get the index for the color
+    const colorIndex = parseInt(chunk, 2);
+
+    // Return the color based on the index
+    return colors[colorIndex % colors.length];
+}
+
+const rotatedChunkArray = (chunkArray) => {
+    const rotatedArray = []
+    for (let i = 0; i < chunkArray.length; i++) {
+        const newPixel = getPixelByIndex(i, chunkArray)
+        rotatedArray.push(newPixel)
+    }
+
+    return rotatedArray
+
+}
+
 
 // Component for rendering the grid
 const Grid = ({ binaryString }) => {
-  // Split the binary string into 3-character chunks
-  const chunks = chunkBinaryString(binaryString);
+    // Split the binary string into 3-character chunks
+    const chunks = chunkBinaryString(binaryString);
+    /// const rotatedChunks = rotatedChunkArray(chunks)
 
-  return (
-    <div style={{ display: 'grid', gridTemplateColumns: `repeat(96, 20px)`, gridGap: '1px' }}>
-      {chunks.map((chunk, index) => (
-        <GridSquare key={index} style={{ backgroundColor: getColorFromChunk(chunk) }} />
-      ))}
-    </div>
-  );
+    return (
+        <div style={{ display: 'grid', gridTemplateColumns: `repeat(96, 20px)`, gridGap: '1px' }}>
+            {chunks.map((chunk, index) => (
+                <GridSquare key={index} style={{ backgroundColor: getColorFromChunk(chunk) }} />
+            ))}
+        </div>
+    );
 };
 
 // Function to map each 3-character chunk to a color
-function getColorFromChunk(chunk) {
-  const colors = [
-    '#000000', // Black
-    '#00FF00', // Green
-    '#FF0000', // Red   
-    '#0000FF', // Color 4
-    '#FFFF00', // Color 5
-    '#FF00FF', // Color 6
-    '#00FFFF', // Color 7
-    '#FFFFFF', // Color 8
-  ];
-
-  // Convert the chunk to a decimal number (base 2) to get the index for the color
-  const colorIndex = parseInt(chunk, 2);
-
-  // Return the color based on the index
-  return colors[colorIndex % colors.length];
-}
 
 export default Grid;
