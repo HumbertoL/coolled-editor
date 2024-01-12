db6e67a65c15b882f6398a1e8d2a76d6bd8b79ef
Humberto <humberto@call-em-all.com>
2024-01-12 09:40:25 -0600

    Fix: make grid smaller

--- src/Grid.js
+++ src/Grid.js
@@ -4,8 +4,8 @@ import { getColorFromChunk } from "./helpers/colors";
 
 // Styled component for the individual grid square
 const GridSquare = styled.div`
-  width: 20px;
-  height: 20px;
+  width: 18px;
+  height: 18px;
   border: 1px solid #ccc;
   font-size: 10px;
   display: grid;
@@ -16,11 +16,11 @@ const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(
     96,
-    20px
+    18px
   ); /* 96 columns, each with a width of 20px */
   grid-template-rows: repeat(
     16,
-    20px
+    18px
   ); /* 16 rows, each with a height of 20px */
   grid-gap: 1px; /* Gap between each grid square */
   grid-auto-flow: column; /* Automatically flow the grid items into columns */
